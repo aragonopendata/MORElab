fa7dd539aff1006e0628983653b5690125717032
Mikel Emaldi <m.emaldi@gmail.com>
2014-09-26 18:03:05 +0200

    Collecting tweets from stream

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 from flask import Flask
 from twitter import *
-from tasks import browse_tuits
+from tasks import browse_tuits, get_stream
 
 app = Flask(__name__)
 
@@ -10,5 +10,6 @@ def index():
     return '[{"user": "foo", "text": "Lorem Ipsum dolosor bla bla bla", "date": "2014-09-24T18:00"}]'
 
 if __name__ == '__main__':
-    browse_tuits.delay()
+    #browse_tuits.delay()
+    #get_stream.delay()
     app.run(debug=True)
--- server/tasks.py
+++ server/tasks.py
@@ -7,6 +7,7 @@ import time
 app = Celery('tasks', broker='redis://localhost:6379/0')
 
 twitter = Twitter(auth=auth)
+twitter_stream = TwitterStream(auth=auth)
 
 conn = psycopg2.connect("dbname=%s user=%s password=%s" % (postgres_db, postgres_user, postgres_pass))
 cur = conn.cursor()
@@ -14,19 +15,20 @@ cur = conn.cursor()
 @app.task
 def browse_tuits():
     max_id = None
-    tweets = twitter.search.tweets(q='', geocode='40.418889,-3.691944,750km', lang='es', count='100')
+    tweets = twitter.search.tweets(q='', geocode='40.418889,-3.691944,750km', lang='es', count='1000')
     for tweet in tweets['statuses']:
         text = tweet['text']
         user = tweet['user']['screen_name']
+        name = tweet['user']['name']
         id = tweet['id']
         lat, long = None, None
         if tweet['coordinates'] != None:
             lat = tweet['coordinates']['coordinates'][1]
             long = tweet['coordinates']['coordinates'][0]
         datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
-        query = "INSERT INTO tweets (id, text, uzers, lat, long, datetime) VALUES (%s, %s, %s, %s, %s, %s)"
+        query = "INSERT INTO tweets (id, text, uzers, name lat, long, datetime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
         try:
-            cur.execute(query, (id, text, user, lat, long, datetime))
+            cur.execute(query, (id, text, user, name, lat, long, datetime))
         except:
             pass
         if max_id == None:
@@ -36,23 +38,25 @@ def browse_tuits():
     conn.commit()
 
     while True:
+        #sleep()
         try:
-            tweets = twitter.search.tweets(q='', geocode='40.418889,-3.691944,750km', lang='es', count='100', max_id=max_id)
+            tweets = twitter.search.tweets(q='', geocode='40.418889,-3.691944,750km', lang='es', count='1000', max_id=max_id)
         except:
             continue
         max_id = None
         for tweet in tweets['statuses']:
             text = tweet['text']
             user = tweet['user']['screen_name']
+            name = tweet['user']['name']
             id = tweet['id']
             lat, long = None, None
             if tweet['coordinates'] != None:
                 lat = tweet['coordinates']['coordinates'][1]
                 long = tweet['coordinates']['coordinates'][0]
             datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
-            query = "INSERT INTO tweets (id, text, uzers, lat, long, datetime) VALUES (%s, %s, %s, %s, %s, %s)"
+            query = "INSERT INTO tweets (id, text, uzers, name lat, long, datetime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
             try:
-                cur.execute(query, (id, text, user, lat, long, datetime))
+                cur.execute(query, (id, text, user, name, lat, long, datetime))
             except:
                 pass
             if max_id == None:
@@ -60,3 +64,25 @@ def browse_tuits():
             elif max_id > tweet['id']:
                 max_id = tweet['id']
         conn.commit()
+
+@app.task
+def get_stream():
+    iterator = twitter_stream.statuses.filter(locations='-180,-90,180,90', language='es')
+
+    for tweet in iterator:
+        text = tweet['text']
+        user = tweet['user']['screen_name']
+        name = tweet['user']['name']
+        id = tweet['id']
+        lat, long = None, None
+        if tweet['coordinates'] != None:
+            lat = tweet['coordinates']['coordinates'][1]
+            long = tweet['coordinates']['coordinates'][0]
+        datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+        query = "INSERT INTO tweets (id, text, uzers, name lat, long, datetime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        print text
+        try:
+            cur.execute(query, (id, text, user, name, lat, long, datetime))
+            conn.commit()
+        except:
+            print 'Error!'
