2b3b8bbe18baa8da7cf0fba6fa808eb0f1640c20
Mikel Emaldi <m.emaldi@gmail.com>
2014-09-27 13:56:52 +0200

    Added pagination to tweets from municipalities

--- server/server.py
+++ server/server.py
@@ -45,6 +45,24 @@ def municipality(municipality):
 
     return json_str
 
+@app.route('/morelab/api/v1.0/municipality/<string:municipality>/<int:page>')
+def municipality_pagination(municipality, page):
+    offset = (page - 1) * 20
+    cur.execute("SELECT * FROM tweets WHERE municipality = '%s' ORDER BY datetime DESC LIMIT 20 OFFSET %s;" % (municipality, offset))
+    items = cur.fetchall()
+    json_str = '['
+    i = 0
+    for item in items:
+        if i == 0:
+            json_str += '{"id": "%s", "text": "%s", "user": "%s", "name": "%s", "lat": "%s", "long": "%s", "datetime": "%s"}' % (item[0], item[1].replace('"', '\\"'), item[2], item[3], item[4], item[5], item[6])
+        else:
+            json_str += ',{"id": "%s", "text": "%s", "user": "%s", "name": "%s", "lat": "%s", "long": "%s", "datetime": "%s"}' % (item[0], item[1].replace('"', '\\"'), item[2], item[3], item[4], item[5], item[6])
+        i += 1
+
+    json_str += ']'
+
+    return json_str
+
 @app.route('/morelab/api/v1.0/stream')
 def index():
     cur.execute("SELECT * FROM tweets ORDER BY datetime DESC LIMIT 20;")
