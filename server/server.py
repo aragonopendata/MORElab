a4d783e41b8bcd68a74fcaef64dcb85903211d76
Mikel Emaldi <m.emaldi@gmail.com>
2014-09-27 11:01:17 +0200

    Added a rest service to get tweets from a concrete municipality

--- server/server.py
+++ server/server.py
@@ -11,6 +11,22 @@ cors = CORS(app)
 conn = psycopg2.connect("dbname=%s user=%s password=%s" % (postgres_db, postgres_user, postgres_pass))
 cur = conn.cursor()
 
+@app.route('/morelab/api/v1.0/municipality/<string:municipality>')
+def municipality(municipality):
+    cur.execute("SELECT * FROM tweets WHERE municipality = '%s' ORDER BY datetime DESC LIMIT 20;" % municipality)
+    items = cur.fetchall()
+    json_str = '['
+    i = 0
+    for item in items:
+        if i == 0:
+            json_str += '{"id": "%s", "text": "%s", "user": "%s", "name": "%s", "lat": "%s", "long": "%s", "datetime": "%s"}' % (item[0], item[1].replace('"', '\\"'), item[2], item[3], item[4], item[5], item[6])
+        else:
+            json_str += ',{"id": "%s", "text": "%s", "user": "%s", "name": "%s", "lat": "%s", "long": "%s", "datetime": "%s"}' % (item[0], item[1].replace('"', '\\"'), item[2], item[3], item[4], item[5], item[6])
+        i += 1
+
+    json_str += ']'
+
+    return json_str
 
 @app.route('/morelab/api/v1.0/stream')
 def index():
